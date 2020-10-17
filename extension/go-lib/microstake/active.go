3c4dd1931115259ece7dfe1aa5fc859aca0032c9
Cliff Young <Cliff@Young.com>
2020-10-17 15:00:28 +0800

    update active

--- extension/go-lib/microstake/active.go
+++ extension/go-lib/microstake/active.go
@@ -3,11 +3,17 @@ package microstake
 import (
 	"fmt"
 	"github.com/hyperion-hyn/hyperion-tf/config"
+	"github.com/hyperion-hyn/hyperion-tf/extension/go-lib/utils"
 	"github.com/hyperion-hyn/hyperion-tf/logger"
-	"time"
 )
 
 func WaitActive() {
-	logger.Log(fmt.Sprintf("sleep %d second for map3Node active", config.Configuration.Network.WaitMap3ActiveTime), true)
-	time.Sleep(time.Duration(config.Configuration.Network.WaitMap3ActiveTime) * time.Second)
+	//logger.Log(fmt.Sprintf("sleep %d second for map3Node active", config.Configuration.Network.WaitMap3ActiveTime), true)
+	//time.Sleep(time.Duration(config.Configuration.Network.WaitMap3ActiveTime) * time.Second)
+	logger.Log(fmt.Sprintf("sleep 1 epoch for map3Node active"), true)
+	rpc, _ := config.Configuration.Network.API.RPCClient()
+	err := utils.WaitForEpoch(rpc, 1)
+	if err != nil {
+		fmt.Printf("Wait for skip epoch error")
+	}
 }
